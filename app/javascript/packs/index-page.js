50899b1af392cf08f7e322e1d0ec3eae98306695
guido caldara <guido.caldara@gmail.com>
2018-10-16 22:01:52 +0800

    ok

--- app/javascript/packs/index-page.js
+++ app/javascript/packs/index-page.js
@@ -6,7 +6,6 @@ import { compose, withProps } from "recompose"
 import {  withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { calculateCenter } from "packs/calculateCenter"
 import { MapWithAMakredInfoWindow } from 'packs/react-map.js';
-import { ButtonsContainer } from 'packs/buttons-container.js'
 import {calcCrow} from 'packs/geoloc-calculator.js'
 import Geocode from "react-geocode";
 Geocode.setApiKey("AIzaSyCFlJdOlbwUvf8Dn0Dov_urzaVvgxdRgxs");
@@ -293,6 +292,70 @@ class IndexPage extends React.Component {
   }
 }
 
+
+
+
+
+
+const ButtonsContainer = (props) => {
+
+  const selectSeason = (e) => {
+    props.selectSeason(e)
+    let boxes = document.querySelectorAll(".season-check")
+    boxes.forEach((b) => {
+      if (b.value != e.target.value) {b.checked = false}
+    })
+  }
+  return (<div className="card filter-row row">
+    <div className="col l5 m6 s12 col-searchbar">
+      <div className="title-search">
+        <p className="search-filter-title">Distanza</p>
+      </div>
+      <div className="d-flex-filter">
+        <div className="checkbox-container">
+          <CheckBox functionClick={props.searchRaces} name="race_distance" value="short" label="Da 0 a 21 Km"/>
+          <CheckBox functionClick={props.searchRaces} name="race_distance" value="medium" label="Da 22 a 42,195 km"/>
+        </div>
+        <div className="checkbox-container">
+          <CheckBox functionClick={props.searchRaces} name="race_distance" value="long" label="Da 42 a 100 Km"/>
+          <CheckBox functionClick={props.searchRaces} name="race_distance" value="ultra" label="Oltre i 100 Km"/>
+        </div>
+      </div>
+    </div>
+    <div className="col l3 m6 s12 col-searchbar">
+      <div className="title-search">
+        <p className="search-filter-title">Tipo</p>
+      </div>
+      <div className="d-flex-filter">
+        <div className="checkbox-container">
+          <CheckBox functionClick={props.searchRaces} name="typo" value="skyrace" label="Skyrace"/>
+          <CheckBox functionClick={props.searchRaces} name="typo" value="vertical" label="Vertical"/>
+        </div>
+        <div className="checkbox-container">
+          <CheckBox functionClick={props.searchRaces} name="typo" value="trail" label="Trail"/>
+          <CheckBox functionClick={props.searchRaces} name="typo" value="race" label="Race"/>
+        </div>
+      </div>
+
+    </div>
+    <div className="col l4 m6 s12 date-column">
+      <div className="title-search">
+        <p className="search-filter-title">Periodo</p>
+      </div>
+      <div className="d-flex-filter">
+        <div className="checkbox-container">
+          <CheckBox functionClick={selectSeason} classval="season-check" name="season" value="summer" data-value = "1" label="Estate"/>
+          <CheckBox functionClick={selectSeason} classval="season-check" name="season" value="spring" data-value = "2"  label="Primavera"/>
+        </div>
+        <div className="checkbox-container">
+          <CheckBox functionClick={selectSeason} classval="season-check" name="season" value="autumn" data-value = "3"  label="Autunno"/>
+          <CheckBox functionClick={selectSeason} classval="season-check" name="season" value="winter" data-value = "4" label="Inverno"/>
+        </div>
+      </div>
+    </div>
+  </div>)
+}
+
 class Place extends React.Component {
   constructor(props) {
     super(props);
@@ -376,6 +439,22 @@ const Stars = (props) =>{
       { h ? <i className="star-blue-card tiny material-icons">star</i> :<i className="star-blue-card tiny material-icons">star_half</i> }
     </div>
   )
+}
+
+
+
+const CheckBox = (props) => {
+  let name = `${props.name}[]`
+  let id = `${props.name}_`
+  let classattributes = `${props.classval} filled-in`
+  return (
+    <label>
+      <div className='checkbox'>
+      <input className={classattributes} onChange={props.functionClick} data-type={props.name} type="checkbox" name={name} id={id} value={props.value}/>
+      <span className="checkBoxSpan">{props.label}</span>
+    </div>
+    </label>
+)
 }
 
 const RaceCard = (props) => {
